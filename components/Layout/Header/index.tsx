92bd255a8b9ea1bb741aeaad4332f4499feb5bcb
Oluka Isaac <hilghsilk2damson@gmail.com>
2022-11-24 02:43:19 +0100

    updated

---
+++ components/Layout/Header/index.tsx
@@ -0,0 +1,13 @@
+import React, { ReactNode } from 'react';
+import Head from 'next/head';
+
+
+const Header = () => {
+  return (
+    <div>
+        Header
+    </div>
+  )
+}
+
+export default Header;
