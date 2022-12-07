f87f7876cdb94a92e6741f6a0a1baad7d1d9055e
Oluka Isaac <hilghsilk2damson@gmail.com>
2022-12-07 05:19:45 +0100

    updated base URL

--- components/constant.ts
+++ components/constant.ts
@@ -1,4 +1,4 @@
-const prod = 'https://timely-liger-06eb9f.netlify.app'
+const prod = 'https://coinfundr.netlify.app'
 const dev = 'http://localhost:3000'
 
 export const  BASE_URL = process.env.NODE_ENV === 'development' ? dev : prod
